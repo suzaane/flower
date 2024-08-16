6d45a84d57502d5ef4eb1659f8544c4e59257b67
Suzaane <suzaynshah@gmail.com>
2024-08-16 16:28:36 +0545

    Add files via upload
    updates

--- flo.js
+++ flo.js
@@ -1,9 +1,14 @@
 //lets start
-
-function hello(){
-    console.log("Hello!");
-}
-hello();
-
-const label=document.getElementById("label");
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
